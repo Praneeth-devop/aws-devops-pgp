c3d8d77939e68c45d4dec9f9f67972ae548b4bfa
Praneeth-devop <praneethnidamanuri50@gmail.com>
2025-03-25 20:17:52 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,13 +1,1 @@
-bash
-#!/bin/bash
-
-# Start your application (replace with your actual application start command)
-cd /home/ubuntu/aws-devops-pgp
-
-# Check if the application is already running
-if pgrep -f "app.py" > /dev/null; then
-  echo "Application is already running."
-else
-  python3 app.py &
-  echo "Application started successfully."
-fi
+sudo python3 app.py
